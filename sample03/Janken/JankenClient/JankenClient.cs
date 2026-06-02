9537da361ff03d98497034b8ba6d2cd43e51420e
Jun Shimura <j.shimura@sugawara.ac.jp>
2026-06-02 17:47:18 +0900

    Janken Base

---
+++ sample03/Janken/JankenClient/JankenClient.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Common;
+
+
+namespace JankenClient
+{
+    internal class JankenClient
+    {
+        public static void Main()
+        {
+            //今回送るHello World!
+            string st = "Hello World!Shimura";
+            Console.WriteLine("lientErrorHandling");
+            SocketClient(st);
+            Console.ReadKey();
+        }
+
+
+        public static void SocketClient(string st)
+        {
+            //IPアドレスやポートを設定(自PC、ポート:11000）
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(hostName);
+            IPAddress ipAddress = ipHostInfo.AddressList[2];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            //外部を指定する場合
+            // IPAddress ipAddress = IPAddress.Parse("172.25.91.135");
+            // IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+            //ソケットを作成
+            Socket socket = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            //接続する。失敗するとエラーで落ちる。
+            try
+            {
+                socket.Connect(remoteEP);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Connect Faild{e.ToString()}");
+                return;
+            }
+
+            // ProtocolHandlerを使ってデータを送信
+            Console.WriteLine($"送信データ: {st}");
+            if (!ProtocolHandler.SendData(socket, st))
+            {
+                Console.WriteLine("送信に失敗しました。");
+                socket.Close();
+                return;
+            }
+
+            // ProtocolHandlerを使ってデータを受信
+            var receiveResult = ProtocolHandler.ReceiveData(socket);
+
+            if (!receiveResult.Success)
+            {
+                // エラーが発生した場合
+                Console.WriteLine($"エラー検知: {receiveResult.ErrorMessage}");
+                Console.WriteLine($"エラータイプ: {ProtocolHandler.GetErrorDescription(receiveResult.ErrorType)}");
+            }
+            else
+            {
+                // 正常に受信したデータを表示
+                Console.WriteLine($"受信データ: {receiveResult.Data}");
+            }
+
+            //ソケットを終了している。
+            socket.Shutdown(SocketShutdown.Both);
+            socket.Close();
+        }
+
+    }
+}
---
+++ sample03/Janken/Jankenhost/JankenHost.cs
@@ -0,0 +1,85 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using Common;
+
+namespace Jankenhost
+{
+    internal class Jankenhost
+    {
+        public static void Main()
+        {
+            Console.WriteLine("HostErrorHandling is starting...");
+            SocketServer();
+        }
+
+        public static void SocketServer()
+        {
+            //ここからIPアドレスやポートの設定
+            // 着信データ用のデータバッファー。
+            //byte[] bytes = new byte[1024];
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            Console.WriteLine($"ホスト名: {ipHostInfo.HostName}");
+            Console.WriteLine($"IPアドレス一覧 (取得数: {ipHostInfo.AddressList.Length}):");
+            for (int i = 0; i < ipHostInfo.AddressList.Length; i++)
+            {
+                Console.WriteLine($"  [{i}] {ipHostInfo.AddressList[i]} (AddressFamily: {ipHostInfo.AddressList[i].AddressFamily})");
+            }
+
+            IPAddress ipAddress = ipHostInfo.AddressList[2];
+            Console.WriteLine($"\n使用するIPアドレス: {ipAddress}");
+
+            IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+            Console.WriteLine($"エンドポイント: {localEndPoint}");
+            Console.WriteLine($"  - IPアドレス: {localEndPoint.Address}");
+            Console.WriteLine($"  - ポート番号: {localEndPoint.Port}");
+            Console.WriteLine();
+            //ここまでIPアドレスやポートの設定
+
+            //ソケットの作成
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            //通信の受け入れ準備
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            //通信の確率
+            Socket handler = listener.Accept();
+
+            // データの受信と検証(ProtocolHandlerを使用)
+            var receiveResult = ProtocolHandler.ReceiveData(handler);
+
+            if (!receiveResult.Success)
+            {
+                // エラーが発生した場合
+                Console.WriteLine($"エラー検知: {receiveResult.ErrorMessage}");
+                Console.WriteLine($"エラータイプ: {ProtocolHandler.GetErrorDescription(receiveResult.ErrorType)}");
+
+                // 接続を切り、セッションを終了
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+                listener.Close();
+                return;
+            }
+
+            // 正常に受信したデータを表示
+            Console.WriteLine($"受信データ: {receiveResult.Data}");
+
+            // 大文字に変更
+            string responseData = receiveResult.Data.ToUpper();
+
+            // クライアントにProtocolHandlerを使って返す
+            if (!ProtocolHandler.SendData(handler, responseData))
+            {
+                Console.WriteLine("送信に失敗しました。");
+            }
+            else
+            {
+                Console.WriteLine($"送信データ: {responseData}");
+            }
+
+            //ソケットの終了
+            handler.Shutdown(SocketShutdown.Both);
+            handler.Close();
+        }
+
+    }
+}
